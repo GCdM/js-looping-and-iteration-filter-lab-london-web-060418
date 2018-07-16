7e933f19a0a91e598b2cf141a09a9dd449a2fd6f
GCdM <gabrielcm.br@gmail.com>
2018-07-16 13:52:43 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution in this file
+function findMatching(collection, match) {
+  return collection.filter(function (element) {return element.toLowerCase() === match.toLowerCase();});
+}
+
+function fuzzyMatch(collection, match) {
+  return collection.filter(function (element) {
+    const size = match.length;
+    return element.slice(0, size) === match;
+  })
+}
+
+function matchName(collection, match) {
+  return collection.filter(function (element) {return element.name === match})
+}
